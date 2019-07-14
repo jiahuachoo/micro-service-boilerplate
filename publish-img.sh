a18f71a46682c5173e52cbbaccb14ad936dca5b0
jiahuachoo <jiahua.choo@gmail.com>
2019-07-14 19:16:15 +1000

    added docker login in publis script

--- publish-img.sh
+++ publish-img.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "$docker_hub_pwd" | docker login -u "$docker_hub_user" --password-stdin
+
 # change to the appropriate repo if necessary
 docker push jiahuachoo/micro-service-boilerplate:latest
